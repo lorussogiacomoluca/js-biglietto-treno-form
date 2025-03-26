f215ea2bfb8f20213040763de067a960c43a78f4
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-26 15:17:46 +0100

    [UPD]  Milestone #1 - Price Function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 const km = document.getElementById('km')
 const age = document.getElementById('age')
 const button = document.getElementById('button')
+
+//Funzione di calcolo del costo
+const calcPrice = (km, age) => {
+    let price;
+    if (age<18){
+        price = (km*0.21)*(0.8)
+    } else if(age>65){
+        price = (km*0.21)*(0.6)
+    }else{
+        price = km*0.21;
+    }
+    return price.toFixed(2);
+}

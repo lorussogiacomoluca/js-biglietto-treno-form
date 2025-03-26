83d58d2fca628eb01acb9fd8aae53851c53543f8
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-26 18:42:22 +0100

    [FINAL] Milestone #3 - Ticket Generation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 //Recupero dati da input
-const km = document.getElementById('km')
-const age = document.getElementById('age')
+const formkm = document.getElementById('km')
+const formage = document.getElementById('age')
+const formnome = document.getElementById('nome')
 const button = document.getElementById('button')
 const form = document.querySelector('form')
 const finalPrice = document.querySelector('.finalPrice')
+const ticketSection = document.getElementById('ticketSection')
+const ticketName = document.getElementById('ticketName')
+const ticketKm = document.getElementById('ticketKm')
+const ticketTariffa = document.getElementById('ticketTariffa')
+const initialPrice  = document.getElementById('initialPrice ')
+const totalDiscount = document.getElementById('totalDiscount')
 
 //Funzione di calcolo del costo
 const calcPrice = (km, age) => {
@@ -21,12 +28,38 @@ const calcPrice = (km, age) => {
 //Recupero dati dopo button
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    input_km = km.value.trim();
-    input_age = age.value.trim();
+    input_km = formkm.value.trim();
+    input_age = formage.value.trim();
+    input_name = formnome.value.trim()
     alert(`Dati ricevuti`)
-    km.value='';
-    age.value='';
+    formkm.value='';
+    formage.value='';
     const res = calcPrice(input_km, input_age);
     console.log(res);
-    finalPrice.textContent = res
+    finalPrice.textContent = res;
+    ticketName.textContent = input_name;
+    ticketKm.textContent = input_km;
+    ticketSection.classList.remove('d-none')
+    initialPrice.textContent = `€ ${(input_km*0.21).toFixed(2).replace('.',',')}`
+
+    let tariffa = '';
+    if (input_age < 18){
+        tariffa = `Young -20%`;
+    }else if(input_age > 65){
+        tariffa = `Senior -40%`
+    }else{
+        tariffa = `Standard`
+    }
+
+    ticketTariffa.textContent=tariffa;
+
+    if (tariffa == 'Standard' && ticketTariffa) {
+        initialPrice.classList.remove('text-decoration-line-through');
+        totalDiscount.textContent = '€ 0,00'
+    }else{
+        let numericValue = parseFloat(res.replace('€', '').replace(' ', '').replace(',', '.'));
+        let sconto = (input_km*0.21) - numericValue;
+        totalDiscount.textContent = `€ ${(sconto).toFixed(2).replace('.',',')}`
+    }
+       
 })
